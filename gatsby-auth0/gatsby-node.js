3227e1efde7a4f45645b11e1d45fb3f4409470f0
unknown <dmt5dh@virginia.edu>
2020-02-10 20:55:53 -0500

    initiated auth0

--- gatsby-auth0/gatsby-node.js
+++ gatsby-auth0/gatsby-node.js
@@ -12,3 +12,25 @@ exports.onCreatePage = async ({ page, actions }) => {
       createPage(page)
     }
   }
+
+  exports.onCreateWebpackConfig = ({ stage, loaders, action }) => {
+    if (stage ==="build-html") {
+      /*
+     * During the build step, `auth0-js` will break because it relies on
+     * browser-specific APIs. Fortunately, we don’t need it during the build.
+     * Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+     * during the build. (See `src/utils/auth.js` to see how we prevent this
+     * from breaking the app.)
+     */
+     actions.setWebpackCOnfig({
+        module: {
+          rules: [
+            {
+              test: /auth0-js/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
