edbe7469645f3a8dcb0c63d31a078def8497c462
unknown <dmt5dh@virginia.edu>
2020-03-01 12:31:35 -0500

    make about us page responsive

--- gatsby-auth0/src/components/aboutUs/teamEntry.js
+++ gatsby-auth0/src/components/aboutUs/teamEntry.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 export default ({name, position, background}) => (
-    <div className="py-4 px-10">
-        <img className="rounded w-64 content-center" src={'/assets/avatar.jpg'} />
-        <div className="text-center p-2">
+    <div className="py-4 px-10 text-center">
+        <img className="block mx-auto rounded w-64 content-center" src={'/assets/avatar.jpg'} />
+        <div className="p-2">
             <h1 className="font-semibold">{name}</h1>
             <h2 className="italic">{position}</h2>
             <h2 className="italic">{background}</h2>
