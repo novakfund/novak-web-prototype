b90f73945381e062fd8f9b84747454b4aab48b32
unknown <dmt5dh@virginia.edu>
2020-03-01 12:34:00 -0500

    fixed footer not sticking to bottom of page

--- gatsby-auth0/src/components/layout.js
+++ gatsby-auth0/src/components/layout.js
@@ -7,7 +7,7 @@ import Footer from "./footer"
 export default ({ children }) => (
     <div className="flex flex-col">
         <Navigation />
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col min-h-screen">
             {children}
         </div>
         <Footer />
