ec7957a3f4442ecb29a26fe85e7f597c5500064d
unknown <dmt5dh@virginia.edu>
2020-03-01 21:10:49 -0500

    added 404 page

---
+++ gatsby-auth0/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <div className="flex justify-center mx-auto pt-8">
+            <h1 className="text-2xl font-semibold">D'oh! Looks like this page doesn't exist!</h1>
+        </div>
+    </Layout>
+)
