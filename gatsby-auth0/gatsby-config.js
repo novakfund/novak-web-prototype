ef07b26928c91b4db52d08d69a2bc699724f0917
unknown <dmt5dh@virginia.edu>
2020-03-01 22:33:03 -0500

    update stuff for deployment

--- gatsby-auth0/gatsby-config.js
+++ gatsby-auth0/gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`],
+  siteMetadata: {
+    title: `NOVAK Fund`,
+    description: `NOVAK Fund website`,
+    author: `NOVAK LLC`
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `NOVAK Fund`,
+        short_name: `Novak`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#FFFFFF`,
+        display: `standalone`,
+        icon: `src/images/novaklogo-small.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
